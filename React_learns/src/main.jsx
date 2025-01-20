21a6647449c30714afc75edda81effd23a2ebd58
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-01-20 22:54:48 +0530

    move App and Greet components to a new directory; update import paths in main entry point

--- React_learns/src/main.jsx
+++ React_learns/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx"; // the App component that is made in the App.jsx file is now imported here for use
-import Greet from "./Greet.jsx";
+import App from "./components/App.jsx"; // the App component that is made in the App.jsx file is now imported here for use
+import Greet from "./components/Greet.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
