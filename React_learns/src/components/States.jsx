d0bf370aa0974110e70f92e71fe3c4474c2c12ac
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-14 02:13:21 +0530

    add friend management features to States component; implement remove and update friend functionality; enhance movie state management with ratings update

--- React_learns/src/components/States.jsx
+++ React_learns/src/components/States.jsx
@@ -6,7 +6,30 @@ const States = () => {
   // by this function only you can change the state of the component
   const [friends, setFriends] = useState(["Alex", "John", "Sparta", "Sakai"]); // taking an array now
 
-  const addNewFriend = () => setFriends([...friends, "Ram"]) // declared my onClick function here that adds new friend in the friends array 
+  const addNewFriend = () => setFriends([...friends, "Ram"]); // declared my onClick function here that adds new friend in the friends array
+
+  const removeFriend = () => setFriends(friends.filter((f) => f !== "Ram"));
+  // declared the removeFriend func for my onClick event
+  // using the filter method to filter out the element
+
+  const updateFriend = () =>
+    setFriends(friends.map((f) => (f === "Sakai" ? "Ghost Sakai Jin" : f))); // declared my update function here for my onClick event
+  // maps over the friends array and find the matching element, if found then changes the element
+
+  const [movie, setMovie] = useState({
+    title: "Interstellar",
+    ratings: 8.9,
+  }); // now using the hook with objects and changing the state of this object
+
+  const changeRating = () => {
+    const copyMovie = {
+      ...movie,
+      ratings : 9.4
+    } // making a copy of the previous object and modifying the data in it
+
+    setMovie(copyMovie) // then passing the new object in here just like we did in counter
+    setMovie({...movie, ratings : 9.8}) // doing all those things here 
+  }
 
   return (
     <section>
@@ -16,12 +39,18 @@ const States = () => {
         <button onClick={() => setCounter(counter - 1)}>-</button>
       </div>
 
-      {friends.map(f => (
+      {friends.map((f) => (
         <li key={Math.random()}>{f}</li>
       ))}
 
       <button onClick={addNewFriend}>Add new friend!</button>
       <button onClick={removeFriend}>Remove friend!</button>
+      <button onClick={updateFriend}>Update friend!</button>
+
+      <h1>Title: {movie.title}</h1>
+      <h3>Ratings: {movie.ratings}</h3>
+
+      <button onClick={changeRating}>Change rating!!</button>
     </section>
   );
 };
