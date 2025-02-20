a70c65fe33da7e0888e007980c369740063b150b
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-20 07:55:28 +0530

    add ShoppingList component; implement item input handling and form submission

--- React_learns/src/components/ShoppingList.jsx
+++ React_learns/src/components/ShoppingList.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ShoppingList = () => {
+  const [items,setItems] = useState([])
+  const [name, setName] = useState("")
+  const [quantity, setQuantity] = useState("")
   return (
-    <div>ShoppingList</div>
+    <div>
+      <h1>Shopping List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="number" value={quantity} onChange={(e) => setName(e.target.value)} /> 
+
+        <button type='submit'>Add items</button>
+
+      </form>
+    </div>
   )
 }
 
