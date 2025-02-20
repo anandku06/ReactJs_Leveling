042dcea1def77cb6bc579ebc26dc20bbef9412d8
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-20 08:08:48 +0530

    refactor ShoppingList component; enhance form submission handling and improve code readability

--- React_learns/src/components/ShoppingList.jsx
+++ React_learns/src/components/ShoppingList.jsx
@@ -1,38 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ShoppingList = () => {
-  const [items,setItems] = useState([])
-  const [name, setName] = useState("")
-  const [quantity, setQuantity] = useState("")
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
 
-  const handleSubmit = () => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    if(!name || !quantity) return
+    if (!name || !quantity) return;
 
     const newItem = {
-      name, quantity : parseInt(quantity)
-    }
+      name,
+      quantity: parseInt(quantity),
+    };
 
-    setItems((prevItems) => [...prevItems, newItem])
-    setName("")
-    setQuantity("")
-  }
+    setItems((prevItems) => [...prevItems, newItem]);
+    setName("");
+    setQuantity("");
+  };
 
   return (
     <div>
       <h1>Shopping List</h1>
       <form onSubmit={handleSubmit}>
-        Name: 
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Item Name' />
-        Quantity: 
-        <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} placeholder='Quantity' /> 
-
-        <button type='submit'>Add items</button>
-
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Item Name"
+        />
+        Quantity:
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          placeholder="Quantity"
+        />
+        <button type="submit">Add items</button>
       </form>
-    </div>
-  )
-}
 
-export default ShoppingList
+      <ul>
+        {items.map((item, i) => (
+          <li key={i}>
+            {item.name} -&gt; Quantity: {item.quantity}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ShoppingList;
