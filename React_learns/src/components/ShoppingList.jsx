683e17fa826e4c4f5f91cb96d1740f2da0334733
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-02-20 08:01:31 +0530

    fix ShoppingList component; correct form submission handling and improve input field accessibility

--- React_learns/src/components/ShoppingList.jsx
+++ React_learns/src/components/ShoppingList.jsx
@@ -4,12 +4,29 @@ const ShoppingList = () => {
   const [items,setItems] = useState([])
   const [name, setName] = useState("")
   const [quantity, setQuantity] = useState("")
+
+  const handleSubmit = () => {
+    e.preventDefault()
+
+    if(!name || !quantity) return
+
+    const newItem = {
+      name, quantity : parseInt(quantity)
+    }
+
+    setItems((prevItems) => [...prevItems, newItem])
+    setName("")
+    setQuantity("")
+  }
+
   return (
     <div>
       <h1>Shopping List</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        <input type="number" value={quantity} onChange={(e) => setName(e.target.value)} /> 
+        Name: 
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Item Name' />
+        Quantity: 
+        <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} placeholder='Quantity' /> 
 
         <button type='submit'>Add items</button>
 
